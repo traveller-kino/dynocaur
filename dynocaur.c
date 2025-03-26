525de51a026188df42d9e24334e37f89d87badc1
Kino <84192243+traveller-kino@users.noreply.github.com>
2025-03-26 14:10:32 -0400

    array fix, axels

--- dynocaur.c
+++ dynocaur.c
@@ -14,17 +14,24 @@ typedef struct Powerplant {
 } Powerplant;
 
 typedef struct Transmission {
-    double[20] gearRatios; // 3.0 -> 2.0 -> 1.5 -> 1.2 -> 1.0 -> 0.85 -> 0.70
+    double gearRatios[8]; // -3.0 -> 3.0 -> 2.0 -> 1.5 -> 1.2 -> 1.0 -> 0.85 -> 0.70
     double differentialRatio; // 3.11
 } Transmission;
 
-typedef struct Surfaces {
+typedef struct Axel {
     double combinedStaticFriction; // 1.0
     double combinedKineticFriction; // 0.7
-    double[4] sagitalGees;
-    double[4] lateralGees;
-    double[4] combinedGees;
+    double sagitalGees[2];
+    double lateralGees[2];
+    double combinedGees[2];
     double gripLimit; // 0.75
+    bool powered;
+    bool soft_spinning; // Exceeding the grip limit by a small amount causes the friction coefficients to drop to 0.1 (ice-like) randomly, for each physics tick
+    bool hard_slipping; // Well exceeding the grip limit will set the friction coefficients to 0.1 no matter what
+} Axel;
+
+typedef struct Surfaces {
+    Axel axels[2]; // Sticking with a normal car for now
 } Surfaces;
 
 typedef struct Aerodynamics {
@@ -33,6 +40,7 @@ typedef struct Aerodynamics {
 
 typedef struct Environment {
     bool wet;
+    bool ice;
 } Environment;
 
 int main(){
